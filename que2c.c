f69eac99cfdd867efbf495adf7a55c7afd20a303
dipak <72122554+issc-dipak@users.noreply.github.com>
2024-09-25 21:34:41 -0700

    Add files via upload

--- que2c.c
+++ que2c.c
@@ -2,34 +2,16 @@
 
 int main()
 {
-    int m,n,i,j,sum=0,rich,max;
-
-    printf("Enter number of Customers: ");
-    scanf("%d",&m);
-    printf("Enter number of Banks: ");
-    scanf("%d",&n);
-    int M1[m][n];
-    for(i=0;i<m;i++){
-        printf("Customer %d : \n",i+1);
-        for(j=0;j<n;j++){
-            printf("\tBank %d : ",j+1);
-            scanf("%d",&M1[i][j]);
-        }
+   int n,m,sum=0;
+   int a[3][4],b[4][3],resut[5][4];
+   printf("entre the element";);
+   for(int i=0 ; i<3;i++){
+    for (int j = 0; j < 4; j++)
+    {
+        sc
     }
     
-    max=sum;
-    for(i=0;i<m;i++){
-        sum=0;
-        for(j=0;j<n;j++){
-            sum+=M1[i][j];
-        }
-        if(max<sum){
-            max=sum;
-            rich=i;
-        }
-    }
-
-    printf(" Customer %d is Richest with Wealth %d.\n",rich+1,max);
+   }
     
     return 0;
 }

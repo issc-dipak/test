90178357af2db64fc45e6c79705e953bc8acf408
dipak <72122554+issc-dipak@users.noreply.github.com>
2024-09-25 21:41:46 -0700

    Add files via upload

---
+++ Test-02-Sept-24/que2c.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+   int n,m,sum=0;
+   int a[3][4],b[4][3],resut[5][4];
+   printf("entre the element";);
+   for(int i=0 ; i<3;i++){
+    for (int j = 0; j < 4; j++)
+    {
+        sc
+    }
+    
+   }
+    
+    return 0;
+}
